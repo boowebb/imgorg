504a130144a1d428dcc0a80ae26e7d3acb0ef38b
Jeff <jeff@tatango.com>
2014-03-26 17:13:38 -0700

    add logic to copy same named file (by incrementing) AND document script file

--- lib/mv_to_dir.rb
+++ lib/mv_to_dir.rb
@@ -13,29 +13,38 @@
 require 'find'
 require 'fileutils'
 require 'mini_exiftool'
-require 'ruby-debug'
+require 'ruby-debug'    # provided in case need to debug
 
 if ARGV.empty?
-  puts 'script requires directory to process'
+  puts "script requires directory to process"
   exit
 end
 
+# only argument required is src_dir - destination is source unless otherwise specified
 src_dir  = ARGV[0]
 dest_dir = ARGV[1] || src_dir
+
+moved_ct  = 0
+stayed_ct = 0
+
+puts
+puts "moving images to YYYY/MM/DD directory structure"
+puts
 
 # recurse over all files in a directory
 Find.find(src_dir).each do |img_path|
 
+  # find gets both dir and files - only process files
   if File.file?(img_path)
     begin
       img_data = MiniExiftool.new(img_path)
 
+      # get image data from meta data in the following order
       img_dt = img_data['DateTimeOriginal'] || img_data['CreateDate'] || img_data['ModifyDate'] || img_data['FileModifyDate']
-      img_dt = img_dt unless img_dt.nil?
 
+      # dump to console if you cannot get date from file (not guaranteed to be an image file)
       if img_dt.nil? || img_dt == ""
-
-        debugger
+        # debugger
         # MiniExiftool.all_tags.select { |t| t =~ /date/i }.map { |x| "#{x} : #{img_data[x]}" }
 
         puts "** not able to get date from file: #{img_path}"
@@ -45,10 +54,20 @@ Find.find(src_dir).each do |img_path|
       new_img_path = "#{dest_dir}/#{img_dt.year}/#{"%02d" % img_dt.month}/#{"%02d" % img_dt.day}/#{img_path.split("/").last}".gsub!('//', '/')
 
       if img_path == new_img_path
+        # just leave image in place if it's where it's supposed to be
+        stayed_ct += 1
 #        puts "-- image does not need to be moved: #{img_path}"
-      elsif File.exists?(new_img_path)
-        puts "!! image already exists in new location: #{img_path}"
       else
+        idx = 0
+
+        # increment file (file_0.gif, file_1.gif, etc) if it exists as named in destination
+        while File.exists?(new_img_path)
+          idx += 1
+          nia = new_img_path.split('.')
+          nia[-2] += "_#{idx}"
+          new_img_path = nia.join(".")
+        end
+
         nda = new_img_path.split('/')
         nda.pop
 
@@ -57,6 +76,7 @@ Find.find(src_dir).each do |img_path|
         FileUtils.mkdir_p new_dir
         FileUtils.mv img_path, new_img_path
 
+        moved_ct += 1
 #        puts ".. moved image #{img_path} to #{new_img_path}"
       end
     rescue Exception => e
@@ -65,3 +85,8 @@ Find.find(src_dir).each do |img_path|
   end
 
 end
+
+puts
+puts "done with move"
+puts ".. moved #{moved_ct} image files"
+puts ".. kept #{stayed_ct} images in current location"
